a9ac8951bcbc24985fb0e8d08e98d22f6b4ec612
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-08-04 20:07:02 +0200

    commit

--- dist/js/index.js
+++
@@ -1,1 +0,0 @@
-import headerFixed from"./modules/headerFixed";import mobileMenu from"./modules/mobileMenu";import initSwiper from"./modules/swiper";import initAnimation from"./modules/animationElements";import"../main.css";document.addEventListener("DOMContentLoaded",(()=>{headerFixed(),mobileMenu(),initSwiper(),initAnimation()})),import.meta.webpackHot&&import.meta.webpackHot.accept();
