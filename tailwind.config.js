c26b870c8f583bc2cd9e2236ce59026d74b51192
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-08-05 10:41:47 +0200

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       center: true,
       padding: {
         DEFAULT: "15px",
+        xl: "0px",
       },
       screens: {
         lg: "768px",
@@ -24,12 +25,43 @@ module.exports = {
     },
     extend: {
       colors: {
+        titleH2: "#BDBDBD",
         bgHeader: "#00183C",
         bgHeaderFixed: "#F9F9F9",
         navHeader: "#333",
         bgFooter: "#2C2C2C",
         colorTextBtn: "#333",
         bgHoverBtn: "#F9F9F9",
+        bgAboutBox: "#FBFBFB",
+      },
+      animation: {
+        "animate-1": "fade-in-el 1s ease-out",
+        "animate-2": "slide-up-el 1s ease-out",
+        "animate-3": "slide-down-el 1s ease-out",
+        "animate-4": "slide-left-el 1s ease-out",
+        "animate-5": "slide-right-el 1s ease-out",
+      },
+      keyframes: {
+        "fade-in-el": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        "slide-up-el": {
+          "0%": { transform: "translateY(100%)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+        "slide-down-el": {
+          "0%": { transform: "translateY(-100%)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+        "slide-left-el": {
+          "0%": { transform: "translateX(100%)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
+        "slide-right-el": {
+          "0%": { transform: "translateX(-100%)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
       },
     },
   },
