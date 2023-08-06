0a5d4aea33fece202f28e6b84db636800065dbc5
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-08-06 20:20:02 +0200

    commit

--- src/js/modules/currentItemMenu.js
+++ src/js/modules/currentItemMenu.js
@@ -1,71 +1,88 @@
+function assignMenuClass(link) {
+  const headerMenuLinks = document.querySelectorAll('.header-menu a');
+  const footerMenuLinks = document.querySelectorAll('.footer-menu a');
+
+  headerMenuLinks.forEach(innerLink => innerLink.classList.remove('current-mrnu-header'));
+  footerMenuLinks.forEach(innerLink => innerLink.classList.remove('current-mrnu-footer'));
+
+  if (link.closest('.header-menu')) {
+    link.classList.add('current-mrnu-header');
+    const matchingFooterLink = document.querySelector(
+      `.footer-menu a[href="${link.getAttribute('href')}"]`
+    );
+    if (matchingFooterLink) {
+      matchingFooterLink.classList.add('current-mrnu-footer');
+    }
+  } else if (link.closest('.footer-menu')) {
+    link.classList.add('current-mrnu-footer');
+    const matchingHeaderLink = document.querySelector(
+      `.header-menu a[href="${link.getAttribute('href')}"]`
+    );
+    if (matchingHeaderLink) {
+      matchingHeaderLink.classList.add('current-mrnu-header');
+    }
+  }
+}
+
 function setActiveLinkByUrl() {
   const currentUrl = window.location.pathname;
-  const normalizedUrl = currentUrl.endsWith('index.html')
-    ? currentUrl.replace('index.html', '')
-    : currentUrl;
+  const currentHash = window.location.hash;
 
   const allLinks = document.querySelectorAll('a');
+  let anchorLinkFound = false;
 
   allLinks.forEach(link => {
     const linkHref = link.getAttribute('href');
-    if (linkHref) {
-      const normalizedLinkHref = linkHref.endsWith('index.html')
-        ? linkHref.replace('index.html', '')
-        : linkHref;
-
-      if (
-        normalizedLinkHref === normalizedUrl ||
-        (normalizedLinkHref === './' && normalizedUrl === '/')
-      ) {
-        if (link.closest('.header-menu')) {
-          link.classList.add('current-mrnu-header');
-        } else if (link.closest('.footer-menu')) {
-          link.classList.add('current-mrnu-footer');
-        }
-      }
+    if (linkHref && linkHref === currentHash) {
+      assignMenuClass(link);
+      anchorLinkFound = true;
     }
   });
+
+  if (!anchorLinkFound) {
+    allLinks.forEach(link => {
+      const linkHref = link.getAttribute('href');
+      if (!linkHref) return;
+      const normalizedLinkHref = linkHref.startsWith('./') ? linkHref.slice(1) : linkHref;
+      if (normalizedLinkHref === currentUrl) {
+        assignMenuClass(link);
+      }
+    });
+  }
 }
+
 export default function currentItemMenu() {
   var headerMenuLinks = document.querySelectorAll('.header-menu a');
   var footerMenuLinks = document.querySelectorAll('.footer-menu a');
 
-  // Функция обработчика для меню header
   headerMenuLinks.forEach(function (link) {
     link.addEventListener('click', function (e) {
-      // Удалить класс active у всех ссылок
-      headerMenuLinks.forEach(innerLink => innerLink.classList.remove('current-mrnu-header'));
-      footerMenuLinks.forEach(innerLink => innerLink.classList.remove('current-mrnu-footer'));
+      headerMenuLinks.forEach(innerLink => innerLink.classList.remove('current-menu-header'));
+      footerMenuLinks.forEach(innerLink => innerLink.classList.remove('current-menu-footer'));
 
-      // Добавить класс для текущей ссылки в header
-      e.currentTarget.classList.add('current-mrnu-header');
+      e.currentTarget.classList.add('current-menu-header');
 
-      // Найти соответствующую ссылку в footer и добавить ей класс
       const matchingFooterLink = document.querySelector(
         `.footer-menu a[href="${e.currentTarget.getAttribute('href')}"]`
       );
       if (matchingFooterLink) {
-        matchingFooterLink.classList.add('current-mrnu-footer');
+        matchingFooterLink.classList.add('current-menu-footer');
       }
     });
   });
 
-  // Функция обработчика для меню footer
   footerMenuLinks.forEach(function (link) {
     link.addEventListener('click', function (e) {
-      // Удалить класс active у всех ссылок
-      headerMenuLinks.forEach(innerLink => innerLink.classList.remove('current-mrnu-header'));
-      footerMenuLinks.forEach(innerLink => innerLink.classList.remove('current-mrnu-footer'));
+      headerMenuLinks.forEach(innerLink => innerLink.classList.remove('current-menu-header'));
+      footerMenuLinks.forEach(innerLink => innerLink.classList.remove('current-menu-footer'));
 
-      // Добавить класс для текущей ссылки в footer
-      e.currentTarget.classList.add('current-mrnu-footer');
+      e.currentTarget.classList.add('current-menu-footer');
 
-      // Найти соответствующую ссылку в header и добавить ей класс
       const matchingHeaderLink = document.querySelector(
         `.header-menu a[href="${e.currentTarget.getAttribute('href')}"]`
       );
       if (matchingHeaderLink) {
-        matchingHeaderLink.classList.add('current-mrnu-header');
+        matchingHeaderLink.classList.add('current-menu-header');
       }
     });
   });
